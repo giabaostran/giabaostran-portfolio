a0f5617c9f6c1ae75b4880f80affb6f01a603aae
giabaostran <sttr2130@gmail.com>
2026-04-23 11:13:38 -0400

    update color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav>
       <div className="nav-left">
-        <div className="nav-avatar-wrap" >
+        <div className="nav-avatar-wrap">
           <div className="nav-avatar-bg" id="avatar-bg">
             <svg
               viewBox="0 0 24 30"
